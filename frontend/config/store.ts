25267146e540a2f7f2b1e723987a748e18e1a809
Marcus <txk.marcus@gmail.com>
2021-12-24 17:34:52 +0800

    Add redux persist

--- frontend/config/store.ts
+++ frontend/config/store.ts
@@ -1,23 +1,39 @@
-import { applyMiddleware, createStore, Middleware, StoreEnhancer } from 'redux';
+import { applyMiddleware, createStore, Middleware, StoreEnhancer, Store } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createWrapper } from 'next-redux-wrapper';
-
+import { persistStore, persistReducer, Persistor } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import rootReducer from 'redux-saga/reducer';
 import rootSaga from 'redux-saga/saga';
 
 const bindMiddleware = (middleware: Middleware[]): StoreEnhancer => {
-  // if (process.env.NODE_ENV !== 'production') {
-  //   const { composeWithDevTools } = require('redux-devtools-extension');
-  //   return composeWithDevTools(applyMiddleware(...middleware));
-  // }
   return applyMiddleware(...middleware);
 };
 
+export interface StoreProperties {
+  persistor?: Persistor;
+}
+
 export const makeStore = () => {
   const sagaMiddleware = createSagaMiddleware();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const store: any = createStore(rootReducer, bindMiddleware([sagaMiddleware]));
-  store.sagaTask = sagaMiddleware.run(rootSaga);
+  let store: Store & StoreProperties;
+  const isServer = typeof window === 'undefined';
+
+  if (isServer) {
+    // If it's on server side, create a store
+    store = createStore(rootReducer, bindMiddleware([sagaMiddleware]));
+    sagaMiddleware.run(rootSaga);
+    return store;
+  }
+
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  store = createStore(persistedReducer, bindMiddleware([sagaMiddleware]));
+  store.persistor = persistStore(store);
+  sagaMiddleware.run(rootSaga);
   return store;
 };
 
